1c67641dae04e1dae2ef644b0412d27a5fe3afa9
darkwraith <jimbenson1337@gmail.com>
2017-06-04 14:43:08 -0400

    Add multiplot function to group multiple plots into one figure based on an NxN matrix. Code used from cookbook-r. Attribution http://www.cookbook-r.com/Graphs/Multiple_graphs_on_one_page_(ggplot2)/

--- R/plotting_functions.R
+++ R/plotting_functions.R
@@ -606,3 +606,60 @@ arbin_dQdV_multi<-function (list,title,cycle=1,ymin=-1000,ymax=1500)
     scale_y_continuous(limits = c(ymin, ymax))
   return(p)
 }
+
+# Multiple plot function, coppied from http://www.cookbook-r.com/Graphs/Multiple_graphs_on_one_page_(ggplot2)/
+#
+# ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
+# - cols:   Number of columns in layout
+# - layout: A matrix specifying the layout. If present, 'cols' is ignored.
+#
+# If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
+# then plot 1 will go in the upper left, 2 will go in the upper right, and
+# 3 will go all the way across the bottom.
+#
+#' Combine plots from many arbin plots into one for ease of report creation
+#'
+#' @param ... list of plot variables saved from an arbin plot function
+#' @param labels character vector with the legends to use in the plots
+#' @export
+#' @examples
+#' p1<-arbin_Qplot(l1[1:3],c("Cell1", "Cell2","Cell3"))
+#' p2<-arbin_Qplot(l1[4:6],c("Cell4", "Cell5","Cell6"))
+#' p3<-arbin_quickplot(l1[[1]],x="Q.d",y="E",norm="mass"))
+#' multiplot(p1,p2,p3,cols=1)
+#' multiplot(p1,p2,p3,cols=2)
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  library(grid)
+
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+
+  numPlots = length(plots)
+
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+
+  if (numPlots==1) {
+    print(plots[[1]])
+
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
